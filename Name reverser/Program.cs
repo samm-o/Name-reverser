a84d302e82d36b38a2885dcb046e5c1b8d80f934
samm-o <samuilsgeorgiev@gmail.com>
2023-01-30 09:55:44 -0500

    Add project files.

---
+++ Name reverser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Name_reverser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type in your name to be reversed: ");
+            var input = Console.ReadLine();
+
+            var name = input.ToCharArray();
+            Array.Reverse(name);
+            var reversedname = string.Join("", name);
+
+            Console.WriteLine(reversedname);
+        }
+    }
+}
